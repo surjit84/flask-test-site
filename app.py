855440a9b275449b5be12e2aad5efbe9b5bbc667
surjit84 <surjit84.singh@gmail.com>
2023-11-29 16:31:42 +0000

    Updated parameter and loop and condition over parameter

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Python Developer",
+        "location": "New York",
+        "salary": "$" + str(5000)
+    },
+    {
+        "id": 2,
+        "title": "Web Developer",
+        "location": "San Francisco",
+        "salary": "$" + str(4000)
+    },
+    {
+        "id": 3,
+        "title": "Data Scientist",
+        "location": "Chicago",
+        "salary": "$" + str(6000)
+    },
+]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Job Finder")
 
 
 if __name__ == '__main__':
